25993fcde4fb90f5a3a86b713057166b6dc6e0b1
Sergey <93572822+JusteRYT@users.noreply.github.com>
2024-11-16 10:22:45 +0300

    -Добавлена JWT-авторизация с контроллером AuthController

---
+++ src/main/java/com/example/Task_Manager/Utils/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.example.Task_Manager.Utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "20387";
+    private static final  long EXPIRATION_TIME = 1000*60*60*10; //10 часов
+
+    public String generateToken(String username) {
+        Key key = new SecretKeySpec(SECRET_KEY.getBytes(), SignatureAlgorithm.ES256.getJcaName());
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        return !extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+}
---
+++ src/main/java/com/example/Task_Manager/controller/AuthController.java
@@ -0,0 +1,55 @@
+package com.example.Task_Manager.controller;
+
+import com.example.Task_Manager.Utils.JwtUtil;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.AuthenticationException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Контроллер для аутентификации пользователей.
+ */
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+            String token = jwtUtil.generateToken(authRequest.getUsername());
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        }catch (BadCredentialsException e){
+            return ResponseEntity.status(401).body("Invalid credentials");
+        }
+    }
+    @Getter @Setter
+    static class AuthRequest{
+        private String username;
+        private String password;
+    }
+}
