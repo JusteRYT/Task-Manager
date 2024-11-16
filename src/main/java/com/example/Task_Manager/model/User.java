a7ff1f14c19686a5cf127d680fa2947aad890d12
Sergey <93572822+JusteRYT@users.noreply.github.com>
2024-11-16 08:50:24 +0300

    -Расписал класс для документации

--- src/main/java/com/example/Task_Manager/model/User.java
+++ src/main/java/com/example/Task_Manager/model/User.java
@@ -5,20 +5,44 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-/*
-Класс сущность пользователь
+/**
+ * Класс `User` представляет пользователя в системе управления задачами.
+ *
+ * <p>Каждый пользователь имеет:
+ * <ul>
+ *   <li>Имя пользователя</li>
+ *   <li>Пароль</li>
+ *   <li>Список задач, связанных с ним</li>
+ * </ul>
+ * </p>
  */
 @Entity
 @Getter
 @Setter
 public class User {
+
+    /**
+     * Уникальный идентификатор пользователя.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    /**
+     * Имя пользователя (уникальное).
+     */
+    @Column(unique = true, nullable = false)
     private String username;
+
+    /**
+     * Пароль пользователя.
+     */
+    @Column(nullable = false)
     private String password;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    /**
+     * Список задач, связанных с пользователем.
+     */
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Task> tasks;
 }
